45d058933e93efe9d6abd44eb04d422e82d6d3c4
vamsikalyan <kalyanvamsi16@gmail.com>
2025-02-26 22:34:08 +0530

    Intial commit

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "vamsi:: Hi sai, how are you?"
+echo "sai:: Hello vamsi, Iam good. what are you doing?"
+echo "vamsi:: Iam doing good sai. what's going on?"
+echo "sai:: I am starting learing shell scripting"
+echo "vamsi:: ohhh good...all the best"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=vamsi 
+PERSON2=sai
+
+echo "$PERSON1:: Hi ${PERSON2}, how are you?"
+echo "${PERSON2}:: Hello $PERSON1, Iam good. what are you doing?"
+echo "$PERSON1":: Iam doing good ${PERSON2}. what's going on?"
+echo "${PERSON2} :: I started learing shell script $PERSON1"
