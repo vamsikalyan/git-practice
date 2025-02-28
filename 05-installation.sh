9a2496c79ac931ac6a738f668f20e8df2d936ddb
vamsikalyan <kalyanvamsi16@gmail.com>
2025-02-28 16:35:41 +0530

    functions

--- 05-installation.sh
+++ 05-installation.sh
@@ -1,17 +1,12 @@
 #!/bin/bash 
  
-# USERID=$(id -u)
-# #echo "User ID is: $USERID"
+USERID=$(id -u)
+#echo "User ID is: $USERID"
 
-# if [ $USERID -ne 0 ]
-# then 
-#      echo "please run this script with root privileges"
-# fi
+if [ $USERID -ne 0 ]
+then 
+     echo "please run this script with root privileges"
+     exit 1
+fi
 
-# dnf install git -y
-
-ls -ltr
-
-lsssgf -ltr
-
-ls -lt
+dnf install git -y
