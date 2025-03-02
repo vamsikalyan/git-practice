dd46540687e148ab9f4301c0b9d58057913794b0
vamsikalyan <kalyanvamsi16@gmail.com>
2025-03-02 13:00:10 +0530

    functions

--- 06-functions.sh
+++ 06-functions.sh
@@ -6,9 +6,9 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2 is failed..."
+        echo "$2 is.. FAILED"
         exit 1
-        echo "$2 is success..."
+        echo "$2 is.. SUCCESS"
      fi    
 }
 
@@ -18,7 +18,7 @@ then
      exit 1
 fi
 
-list installed git 
+dnf list installed git 
 
 VALIDATE $? "Lisiting git"
 
