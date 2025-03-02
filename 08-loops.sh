1b7b173cc3da94d2196b6f0520ef7f8d35b8b635
vamsikalyan <kalyanvamsi16@gmail.com>
2025-03-02 17:57:42 +0530

    functions

--- 08-loops.sh
+++ 08-loops.sh
@@ -3,6 +3,14 @@
 
 # sh 08-loops.sh git mysql postfix nginx 
 for package in $@ # refers to all arguments to passed it.
-do
-   echo $package
-done    
+do  
+   dnf list installed $package
+   if [ $? -ne 0 ]
+   then
+       echo "$package is not installed, goint to install it.
+       dnf install $package -y
+       VALIDATE $? "Installing $package"
+   else
+       echo "$package is already installed..nothing to do"
+done
+
